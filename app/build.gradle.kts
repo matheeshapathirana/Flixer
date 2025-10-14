966f001d9ab80aa8ca55b00506d6e13c8c00257c
Sanupa <sanuparanmeth@gmail.com>
2025-10-15 01:37:24 +0530

    Added internet permissions to AndroidManifest.xml Created bottom navigation menu and home, watchlist and profile fragments. Implemented bottom navigation menu to change fragments when the icon is clicked.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,11 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -38,6 +43,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/matheesha/flixer/MainActivity.java
+++ app/src/main/java/com/matheesha/flixer/MainActivity.java
@@ -7,18 +7,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.matheesha.flixer.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment()); //create a home fragment on app launch
+
+
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Change fragment when the user taps on the icon in the bottom nav
+        binding.bottomNav.setOnItemSelectedListener(item -> {
+            int itemID = item.getItemId();
+
+            if (itemID == R.id.nav_home_icon) {
+                replaceFragment(new HomeFragment());
+            } else if (itemID == R.id.nav_watchlist_icon) {
+                replaceFragment(new WatchlistFragment());
+            } else if (itemID == R.id.nav_profile_icon) {
+                replaceFragment(new ProfileFragment());
+            }
+
+            return true;
+
+        });
+    }
+
+    //Method to replace FrameLayout with the particular fragment
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
