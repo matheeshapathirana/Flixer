78342afb26e3428f2c45c1fb657b322397d498cc
Sanupa <sanuparanmeth@gmail.com>
2025-10-17 10:10:17 +0530

    Created basic tab layout design for watchlist fragment

---
+++ app/src/main/java/com/matheesha/flixer/ViewPagerAdapter.java
@@ -0,0 +1,30 @@
+package com.matheesha.flixer;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+
+    public ViewPagerAdapter(@NonNull WatchlistFragment fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new WatchlistMoviesFragment();
+            case 1:
+                return new WatchlistTvFragment();
+            default:
+                return new WatchlistMoviesFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
