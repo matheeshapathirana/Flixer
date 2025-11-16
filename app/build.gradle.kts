e0608b69fa119e38c0598d3efca2cef15c443b52
matheeshapathirana <pathiranamatheesha@gmail.com>
2025-11-16 16:40:35 +0530

    Refactor build.gradle.kts to add release signing configs and update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 import java.util.Properties
 import java.io.FileInputStream
 
-
 plugins {
     alias(libs.plugins.android.application)
     id("com.google.gms.google-services")
@@ -10,42 +9,55 @@ plugins {
 android {
     namespace = "com.matheesha.flixer"
     compileSdk = 36
+
+    val props = Properties().apply {
+        val propsFile = rootProject.file("local.properties")
+        if (propsFile.exists()) {
+            load(FileInputStream(propsFile))
+        }
+    }
+
     defaultConfig {
         applicationId = "com.matheesha.flixer"
         minSdk = 33
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
     // Load keys from local.properties
     //https://al-e-shevelev.medium.com/a-secure-way-to-store-api-keys-in-android-applications-238135709067
-    val props = Properties()
-    val propsFile = rootProject.file("local.properties")
-    if (propsFile.exists()) {
-        props.load(FileInputStream(propsFile))
+        buildConfigField(
+            "String",
+            "TMDB_ACCESS_TOKEN",
+            "\"${props.getProperty("TMDB_ACCESS_TOKEN", "")}\""
+        )
+        buildConfigField(
+            "String",
+            "OMDB_API_KEY",
+            "\"${props.getProperty("OMDB_API_KEY", "")}\""
+        )
     }
 
-    buildConfigField(
-        "String",
-        "TMDB_ACCESS_TOKEN",
-        "\"${props.getProperty("TMDB_ACCESS_TOKEN", "")}\""
-    )
-    buildConfigField(
-        "String",
-        "OMDB_API_KEY",
-        "\"${props.getProperty("OMDB_API_KEY", "")}\""
-    )
-}
+    signingConfigs {
+        create("release") {
+            storeFile = file(props.getProperty("storeFile", "Keys/release-key"))
+            storePassword = props.getProperty("storePassword")
+            keyAlias = props.getProperty("keyAlias")
+            keyPassword = props.getProperty("keyPassword")
+        }
+    }
 
     buildTypes {
-        release {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+        getByName("debug") {
         }
     }
 
@@ -76,7 +88,7 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
-    // Your app dependencies
+
     implementation("com.android.volley:volley:1.2.1")
     implementation("com.squareup.picasso:picasso:2.8")
     implementation("androidx.cardview:cardview:1.0.0")
@@ -85,17 +97,14 @@ dependencies {
     implementation("androidx.core:core-splashscreen:1.0.1")
 
     // Firebase - FIXED VERSION
-    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.android.gms:play-services-auth:21.2.0")
 
-    // UI Dependencies - REMOVED DUPLICATES
+    // UI Dependencies
     implementation("com.airbnb.android:lottie:6.4.0")  // Updated Lottie version
     implementation("androidx.viewpager2:viewpager2:1.0.0")
     implementation("androidx.fragment:fragment-ktx:1.6.1")
-    implementation("com.android.volley:volley:1.2.1")
-    implementation("com.squareup.picasso:picasso:2.8")
     implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("androidx.fragment:fragment-ktx:1.7.1")
